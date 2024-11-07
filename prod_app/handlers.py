5d4518e91f2d5b798ba14557dd3e9eb655c3135d
vadim_p <vadimp_91@mail.ru>
2024-11-07 11:15:31 +0300

    create, read product add

--- prod_app/handlers.py
+++ prod_app/handlers.py
@@ -5,6 +5,7 @@ from tornado.web import RequestHandler
 from sqlalchemy import insert
 
 from prod_app.models import Product
+from prod_app.queries import create_product, get_product_by_name
 from prod_app.settings import engine, SessionFactory
 
 
@@ -13,37 +14,24 @@ class MainHandler(RequestHandler):
         self.write("Index page")
 
 
-def create_product(name):
-    with SessionFactory() as session:
-        new_product = Product(name=name)  # Создаем объект продукта
-        session.add(new_product)  # Добавляем объект в сессию
-        session.commit()  # Сохраняем изменения в БД
-        product_params = {
-            "id": new_product.id,
-            "name": new_product.name,
-            "category": new_product.category,
-        }
-        return product_params  # Возвращаем объект продукта
-    # stmt = insert(Product).values(name=name)
-    # with engine.connect() as conn:
-    #     result = conn.execute(stmt)
-    #     conn.commit()
-    #     product_id = result.inserted_primary_key[0]
-    #     # print(result.keys())
-    #     return product_id
-
-
 class CreateProductHandler(RequestHandler):
-    def post(self):
+    def post(self) -> None:
         name = self.get_argument("name")
         try:
-            product_params = create_product(name)
+            new_product_params = create_product(name)
             self.set_status(201)
-            self.write(product_params)
+            self.write(new_product_params)
         except Exception as exc:
             self.set_status(500)
             self.write({"error": str(exc)})
 
 
 class GetProductHandler(RequestHandler):
-    pass
+    def get(self, name: str):
+        try:
+            current_product_params = get_product_by_name(name)
+            self.set_status(201)
+            self.write(str(current_product_params))
+        except Exception as exc:
+            self.set_status(500)
+            self.write({"error": str(exc)})
---
+++ prod_app/queries.py
@@ -0,0 +1,41 @@
+from prod_app.models import Product
+from prod_app.settings import SessionFactory
+from sqlalchemy import select
+
+
+# stmt = insert(Product).values(name=name)
+# with engine.connect() as conn:
+#     result = conn.execute(stmt)
+#     conn.commit()
+#     product_id = result.inserted_primary_key[0]
+#     # print(result.keys())
+#     return product_id
+
+def create_product(name: str) -> dict:
+    with SessionFactory() as session:
+        new_product = Product(name=name)
+        session.add(new_product)
+        session.commit()
+        new_product_params = {
+            "id": new_product.id,
+            "name": new_product.name,
+            "category": new_product.category,
+        }
+        return new_product_params
+
+
+def get_product_by_name(name: str):
+    with SessionFactory() as session:
+        stmt = select(Product).where(Product.name == name)
+        current_products = session.scalars(stmt).all()
+        result = []
+        if not current_products:
+            return {}
+        for prod in current_products:
+            prod_params = {
+                "id": prod.id,
+                "name": prod.name,
+                "category": prod.category,
+            }
+            result.append(prod_params)
+        return result
